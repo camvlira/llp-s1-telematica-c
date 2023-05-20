4ec90bb394299a4fe7fd05ea4a469952637391c4
camvlira <127799093+camvlira@users.noreply.github.com>
2023-05-20 17:07:06 +0000

    Primeiro Commit

--- Atividade02.c
+++ Atividade02.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
-void matrixSum(int n, int A[][n], int B[][n], int C[][n]) {
+void matrixSum(int n, int MatA[][n], int MatB[][n], int MatC[][n]) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            C[i][j] = A[i][j] + B[i][j];
+            MatC[i][j] = MatA[i][j] + MatB[i][j];
         }}}
 int main() {
-    int n = 3;
+    int tamanhopMatrix = 3;
     int Mat1[3][3] = {{1, 5, 3}, {2, 5, 1}, {7, 1, 3}};
     int Mat2[3][3] = {{1, 3, 3}, {1, 6, 3}, {3, 4, 1}};
     int MatSum[3][3];
 
-    matrixSum(n, Mat1, Mat2, MatSum);
-    printf("Soma da matrizes:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    matrixSum(tamanhopMatrix, Mat1, Mat2, MatSum);
+    
+    printf("Matrix A:\n");
+    for (int i = 0; i < tamanhopMatrix; i++) {
+        for (int j = 0; j < tamanhopMatrix; j++) {
+            printf("%i ", Mat1[i][j]);
+        }printf("\n");}
+
+        printf("Matriz B:\n");
+    for (int i = 0; i < tamanhopMatrix; i++) {
+        for (int j = 0; j < tamanhopMatrix; j++) {
+            printf("%i ", Mat2[i][j]);
+        }printf("\n");}
+
+        printf("Soma da matrizes:\n");
+    for (int i = 0; i < tamanhopMatrix; i++) {
+        for (int j = 0; j < tamanhopMatrix; j++) {
             printf("%i ", MatSum[i][j]);
         }printf("\n");}
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
